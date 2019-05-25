3522f6817a5771c4e6e2ec61d2980371dce1ea89
Lucas Mendes <lucas.lmmf@gmail.com>
2019-05-25 20:48:37 -0300

    upd ubuntu sh

--- .ubuntu/server.sh
+++ .ubuntu/server.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-echo 'Abrindo.....'
+unset PYTHONPATH
 
-DSUS_EXTRACTOR_ROOT_PATH=$(dirname $(dirname $(dirname $0)))
+DSUS_EXTRACTOR_ROOT_PATH=$(dirname $0)
+PIP_LOCAL=$DSUS_EXTRACTOR_ROOT_PATH/.venv/bin/pip
+PYTHON_LOCAL=$DSUS_EXTRACTOR_ROOT_PATH/.venv/bin/python
 
 cd $DSUS_EXTRACTOR_ROOT_PATH/.src
 
-pip install -r requirements.txt
+$PIP_LOCAL install -r requirements.txt
 
 cd $DSUS_EXTRACTOR_ROOT_PATH/.src/webserver/
 
-python server.py
+echo ">>>>>>>>>>>>>>>>>>>>>>>>INICIANDO APLICACAO<<<<<<<<<<<<<<<<<<<<<<<<"
+
+# executa o segundo sem esperar o primeiro terminar pra abrir a janela qdo o servidor já estiver ok
+$PYTHON_LOCAL open_url.py & $PYTHON_LOCAL server.py
+
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>>Aplicação encerrada. Feche o terminal.<<<<<<<<<<<<<<<<<<<<<<<<"
+
+
 ping localhost
