bf87d70c3ccd489397483579cb49621a27b423eb
Lucas Mendes <lucas.lmmf@gmail.com>
2019-05-25 21:04:45 -0300

    change py exec

--- .ubuntu/server.sh
+++ .ubuntu/server.sh
@@ -3,19 +3,19 @@
 unset PYTHONPATH
 
 DSUS_EXTRACTOR_ROOT_PATH=$(dirname $0)
-PIP_LOCAL=$DSUS_EXTRACTOR_ROOT_PATH/.venv/bin/pip
-PYTHON_LOCAL=$DSUS_EXTRACTOR_ROOT_PATH/.venv/bin/python
+#PIP_LOCAL=$DSUS_EXTRACTOR_ROOT_PATH/.venv/bin/pip
+#PYTHON_LOCAL=$DSUS_EXTRACTOR_ROOT_PATH/.venv/bin/python
 
 cd $DSUS_EXTRACTOR_ROOT_PATH/.src
 
-$PIP_LOCAL install -r requirements.txt
+pip install -r requirements.txt
 
 cd $DSUS_EXTRACTOR_ROOT_PATH/.src/webserver/
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>INICIANDO APLICACAO<<<<<<<<<<<<<<<<<<<<<<<<"
 
 # executa o segundo sem esperar o primeiro terminar pra abrir a janela qdo o servidor já estiver ok
-$PYTHON_LOCAL open_url.py & $PYTHON_LOCAL server.py
+python open_url.py & python server.py
 
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>Aplicação encerrada. Feche o terminal.<<<<<<<<<<<<<<<<<<<<<<<<"
