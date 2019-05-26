21dfc4a6eebef88399df72db5a5f74b4869c78e0
Lucas Mendes <lucas.lmmf@gmail.com>
2019-05-26 20:52:00 -0300

    add py setup for ubuntu

--- .ubuntu/server.sh
+++ .ubuntu/server.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-unset PYTHONPATH
+if ! hash python; then
+    echo "Python não instalado. É necessário possui a versao Python 2.7 ou superior."
+    echo "Para instalar, execute o aplicativo SETUP.desktop e aguarde o término da instalação."
+    echo "Para instalar será solicitada a sua senha da máquina"
+fi
+
+ver=$(python -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
+if [ "$ver" -lt "27" ]; then
+    echo "This script requires python 2.7 or greater"
+    exit 1
+fi
 
 DSUS_EXTRACTOR_ROOT_PATH=$(dirname $(dirname $0))
-PIP_LOCAL=$DSUS_EXTRACTOR_ROOT_PATH/ubuntu-python-src/bin/pip3
-PYTHON_LOCAL=$DSUS_EXTRACTOR_ROOT_PATH/ubuntu-python-src/bin/python3
+
+PYTHON_DSUS_PATH=DSUS_EXTRACTOR_ROOT_PATH/.ubuntu-python-src
+PIP_LOCAL=$PYTHON_DSUS_PATH/bin/pip
+PYTHON_LOCAL=$PYTHON_DSUS_PATH/bin/python
 
 cd $DSUS_EXTRACTOR_ROOT_PATH/.src
 
