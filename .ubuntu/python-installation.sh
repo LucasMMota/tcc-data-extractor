b29dab28965a6f0de29a047b8bad0d85cc74acb3
lucas mendes <lucas.lmmf@gmail.com>
2019-05-26 21:50:35 -0300

    upd sh

--- .ubuntu/python-installation.sh
+++ .ubuntu/python-installation.sh
@@ -1,33 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-
-#########################################
-# Installing python and necessary packages
-# locally. This script will install python
-# into the $PYTHON_DSUS_PATH/bin directory and install
-# numpy + scipy
-#########################################
-DSUS_EXTRACTOR_ROOT_PATH=$(dirname $(dirname $0))
-PYTHON_DSUS_PATH=$DSUS_EXTRACTOR_ROOT_PATH/.ubuntu-python-src
-
-# installing python 2.7.3
-mkdir -p $PYTHON_DSUS_PATH
-wget http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tgz
-tar xvzf Python-2.7.3.tgz
-cd Python-2.7.3
-./configure
-make
-make altinstall prefix=$PYTHON_DSUS_PATH  # specify local installation directory
-ln -s $PYTHON_DSUS_PATH/python2.7 $PYTHON_DSUS_PATH/bin/python
-cd ..
+if ! hash python; then
+	sudo apt-get install python
+fi
 
 # install setuptools and pip for package management
-wget http://pypi.python.org/packages/source/s/setuptools/setuptools-0.6c11.tar.gz#md5=7df2a529a074f613b509fb44feefe74e
-tar xvzf setuptools-0.6c11.tar.gz
-cd setuptools-0.6c11
-$PYTHON_DSUS_PATH/bin/python setup.py install  # specify the path to the python you installed above
-cd ..
-wget http://pypi.python.org/packages/source/p/pip/pip-1.2.1.tar.gz#md5=db8a6d8a4564d3dc7f337ebed67b1a85
-tar xvzf pip-1.2.1.tar.gz
-cd pip-1.2.1
-$PYTHON_DSUS_PATH/bin/python setup.py install  # specify the path to the python you installed above
+if ! hash pip; then
+	sudo apt-get install python-pip
+fi
+
+ping localhost
--- .ubuntu/server.sh
+++ .ubuntu/server.sh
@@ -4,30 +4,29 @@ if ! hash python; then
     echo "Python não instalado. É necessário possui a versao Python 2.7 ou superior."
     echo "Para instalar, execute o aplicativo SETUP.desktop e aguarde o término da instalação."
     echo "Para instalar será solicitada a sua senha da máquina"
+    wait $!
+    exit 1
 fi
 
 ver=$(python -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
 if [ "$ver" -lt "27" ]; then
     echo "This script requires python 2.7 or greater"
+    wait $!
     exit 1
 fi
 
 DSUS_EXTRACTOR_ROOT_PATH=$(dirname $(dirname $0))
 
-PYTHON_DSUS_PATH=DSUS_EXTRACTOR_ROOT_PATH/.ubuntu-python-src
-PIP_LOCAL=$PYTHON_DSUS_PATH/bin/pip
-PYTHON_LOCAL=$PYTHON_DSUS_PATH/bin/python
-
 cd $DSUS_EXTRACTOR_ROOT_PATH/.src
 
-$PIP_LOCAL install -r requirements.txt
+pip install -r requirements.txt
 
 cd $DSUS_EXTRACTOR_ROOT_PATH/.src/webserver/
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>INICIANDO APLICACAO<<<<<<<<<<<<<<<<<<<<<<<<"
 
 # executa o segundo sem esperar o primeiro terminar pra abrir a janela qdo o servidor já estiver ok
-$PYTHON_LOCAL open_url.py & $PYTHON_LOCAL server.py
+python open_url.py & python server.py
 
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>Aplicação encerrada. Feche o terminal.<<<<<<<<<<<<<<<<<<<<<<<<"
