fdea4737522cf6e18aa308e85cc9f80786fb62f4
Lucas Mendes <lucas.lmmf@gmail.com>
2019-05-30 22:46:29 -0300

    win

---
+++ WIN-SETUP.bat
@@ -0,0 +1,17 @@
+#DSUS_EXTRACTOR_ROOT_PATH=$(dirname $(dirname $0))
+#cd $DSUS_EXTRACTOR_ROOT_PATH/.src
+
+pip install Flask
+pip install pandas
+
+# retry
+pip install Flask
+pip install pandas
+#Rscript r-requirements.r
+
+ECHO " "
+ECHO " "
+ECHO " "
+ECHO ">>> Instalação realizada com sucesso! Feche o terminal."
+
+PAUSE
