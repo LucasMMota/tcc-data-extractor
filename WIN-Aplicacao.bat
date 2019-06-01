1069345f53ed60e6fa627f0a16add5484769ea6b
Lucas Mendes <lucas.lmmf@gmail.com>
2019-05-31 23:26:50 -0300

    add win files setup

--- WIN-Aplicacao.bat
+++ WIN-Aplicacao.bat
@@ -1,3 +1,17 @@
-#!/bin/bash
+@ECHO OFF
 
-python -mwebbrowser http://127.0.0.1:5000/
+set DATASUS_WORKINGDIR=%~dp0
+set DATASUS_WEBSERVERDIR=%~dp0.src\webserver
+@SET DSUS_PYPATH=%DATASUS_WORKINGDIR%\win\
+
+CD %DATASUS_WEBSERVERDIR%
+@CD %SUS_PYPATH%
+
+ECHO ">>>>>>>>>>>>>>>>>>>>>>>>INICIANDO APLICACAO<<<<<<<<<<<<<<<<<<<<<<<<"
+
+@ executa o segundo sem esperar o primeiro terminar pra abrir a janela qdo o servidor já estiver ok
+python open_url.py & python server.py
+
+ECHO ">>>>>>>>>>>>>>>>>>>>>>>>Aplicação encerrada. Feche o terminal.<<<<<<<<<<<<<<<<<<<<<<<<"
+
+PAUSE
---
+++ WIN-SETUP2.bat
@@ -0,0 +1,50 @@
+@ECHO OFF
+
+ECHO .
+ECHO .
+ECHO .
+ECHO INICIANDO INSTALACAO DOS REQUISITOS, PODE DEMORAR ALGUNS MINUTOS
+ECHO AGUARDE ATE MENSAGEM DE FINALIZACAO DA INSTALACAO
+ECHO .
+ECHO .
+ECHO .
+
+:: Check for Python Installation
+python --version 2>NUL
+if errorlevel 1 goto errorNoPython
+
+pip --version 2>NUL
+if errorlevel 1 goto errorNoPython
+goto:setupStep
+goto:eof
+
+:errorNoPython
+ECHO ========================ALERTA========================================
+ECHO PYTHON nao esta instalado nesse computador, por favor instale-o atraves
+ECHO do instalador XTPO que pode ser encontrado nesse diretorio
+ECHO Lembre-se de instalar o PIP (em caso de duvidas veja o arquivo "AJUDA.txt"
+ECHO ======================================================================
+
+PAUSE
+
+
+:setupStep
+ECHO "Verificando dependencias do projeto. Aguarde por favor."
+set DATASUS_WORKINGDIR=%~dp0
+set DATASUS_WEBSERVERDIR=%~dp0.src\webserver\
+::SET DSUS_PYPATH=%DATASUS_WORKINGDIR%win\Scripts\
+
+pip install Flask
+pip install pandas
+
+:: retry
+pip install Flask
+pip install pandas
+:: Rscript r-requirements.r
+
+ECHO .
+ECHO .
+ECHO .
+ECHO ">>> INSTALACAO REALIZADA COM SUCESSO! FECHE O TERMINAL."
+
+PAUSE
