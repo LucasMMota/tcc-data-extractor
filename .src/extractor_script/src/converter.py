e963bb92500e06008eaae7ce8bf09f89e5fd039d
Lucas Mendes <lucas.lmmf@gmail.com>
2019-06-01 00:56:10 -0300

    d as da

--- .src/extractor_script/src/converter.py
+++ .src/extractor_script/src/converter.py
@@ -1,6 +1,7 @@
 import subprocess
 import commands
 from .constants import SRC_DIR, RAW_FILES_DIR, CONVERTED_FILES_DIR
+import platform
 
 
 def dbc2csv(raw_filename):
@@ -9,15 +10,26 @@ def dbc2csv(raw_filename):
     tries = 2
     while tries > 0:
         try:
-            R_PATH = commands.getstatusoutput('which Rscript')[1]
-            # todo instalar pacote do R se nao estiver disponivel..
-            print("Rscript path: " + R_PATH) # todo remover
-            print(">>>>>>>>>"+R_PATH + " --vanilla " + dbc2csv_path) # todo remover
+            if(platform.system() == 'Windows'):
+                R_PATH = "C:\Program Files\R\R-3.6.0\\bin\\R.exe"
+                print(">>>>>>>>>" + R_PATH + " --vanilla " + dbc2csv_path)  # todo remover
 
-            result = subprocess.call(R_PATH + " --vanilla " + dbc2csv_path, shell=True)
-            print(result)
-            tries = 0
-            return True
+                result = subprocess.call(R_PATH + " --vanilla " + dbc2csv_path, shell=True)
+                print(result)
+                tries = 0
+                return True
+            else:
+                R_PATH = commands.getstatusoutput('which Rscript')[1]
+                # todo instalar pacote do R se nao estiver disponivel..
+                # fazer bench de qto tempo gasta com ferramentas diferentes
+                # ver tempo que gasta pra instalar qdo for colocar nas isntrucoes
+                print("Rscript path: " + R_PATH) # todo remover
+                print(">>>>>>>>>"+R_PATH + " --vanilla " + dbc2csv_path) # todo remover
+
+                result = subprocess.call(R_PATH + " --vanilla " + dbc2csv_path, shell=True)
+                print(result)
+                tries = 0
+                return True
         except:
             print("(R) Erro ao converter o arquivo: " + raw_filename)
             tries -= 1
